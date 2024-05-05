d7b534a39a552ffd2915a58d2189db1baf9a7dfa
Abdullah <abdullahpsg@gmail.com>
2024-05-05 11:47:06 +0700

    [NEW] docker compose support

--- src/main/java/io/abun/springbootikamers/SellerServices/SellerController.java
+++ src/main/java/io/abun/springbootikamers/SellerServices/SellerController.java
@@ -1,5 +1,23 @@
 package io.abun.springbootikamers.SellerServices;
 
-public class SellerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/sellers")
+public class SellerController {
+    @Autowired
+    SellerService service;
+
+    @PostMapping("/new")
+    public Seller createNewSeller(@RequestBody Seller seller) {
+        return service.createSeller(seller);
+    }
+
+    @GetMapping
+    public Seller getSellerById(@RequestBody UUID sellerId) {
+        return service.findSellerById(sellerId);
+    }
 }
